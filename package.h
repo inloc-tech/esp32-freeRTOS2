3a1a40f85226ea436930f97f9237477798675cf9
zimbora <lucas.ua.eet@gmail.com>
2026-07-30 12:00:44 +0100

    prepares v1.1.15-1.0.9

--- package.h
+++ package.h
@@ -10,7 +10,7 @@
 //                                                                 //
 /////////////////////////////////////////////////////////////////////
 
-#define FW_VERSION	"1.1.14"
+#define FW_VERSION	"1.1.15"
 
 #ifndef ARP_TIMEOUT_MS
 #define ARP_TIMEOUT_MS 3000
